2107388942b7ae18163d02e7122bc2764f06336a
TatyanaKorolevaa <[200~t310kor233@mail.ru~>
2024-02-14 18:56:50 +0300

    HW v.2

--- HW10.py
+++ HW10.py
@@ -9,5 +9,4 @@ data = pd.DataFrame({'whoAmI': lst})
 data_one_hot = data.apply(lambda x: x.astype('category').cat.codes)
 data_one_hot = pd.concat([data_one_hot, 1 - data_one_hot], axis=1)
 data_one_hot.columns = ['human', 'robot']
-
 print(data_one_hot)
